7a9701fbc95212bc63edec4bf43688a3c2a0e471
Jordan-low <jawdanlow@gmail.com>
2021-10-26 22:00:27 +0800

    Add project files.

---
+++ App/Source/Scene3D/Scene3D.cpp
@@ -0,0 +1,214 @@
+/**
+ CScene3D
+ By: Toh Da Jun
+ Date: Mar 2020
+ */
+#include "Scene3D.h"
+
+// Include GLEW
+#ifndef GLEW_STATIC
+	#define GLEW_STATIC
+	#include <GL/glew.h>
+#endif
+
+// Include this for glm::to_string() function
+#define GLM_ENABLE_EXPERIMENTAL
+#include <includes/gtx/string_cast.hpp>
+
+// Include filesystem to read from a file
+#include "System\filesystem.h"
+
+// Include CShaderManager
+#include "RenderControl/ShaderManager.h"
+
+#include <iostream>
+using namespace std;
+
+/**
+ @brief Constructor This constructor has protected access modifier as this class will be a Singleton
+ */
+CScene3D::CScene3D(void)
+	: cSettings(NULL)
+	, cKeyboardController(NULL)
+	, cMouseController(NULL)
+	, cFPSCounter(NULL)
+	, cSoundController(NULL)
+	, cCamera(NULL)
+	, cSkybox(NULL)
+	, cTerrain(NULL)
+{
+}
+
+/**
+ @brief Destructor
+ */
+CScene3D::~CScene3D(void)
+{
+	// Destroy the camera
+	if (cCamera)
+	{
+		cCamera->Destroy();
+		cCamera = NULL;
+	}
+
+	// Destroy the cSoundController
+	if (cSoundController)
+	{
+		cSoundController->Destroy();
+		cSoundController = NULL;
+	}
+
+	// Destroy the skybox
+	if (cSkybox)
+	{
+		cSkybox->Destroy();
+		cSkybox = NULL;
+	}
+
+	// Destroy the terrain
+	if (cTerrain)
+	{
+		cTerrain->Destroy();
+		cTerrain = NULL;
+	}
+
+	// We won't delete this since it was created elsewhere
+	cFPSCounter = NULL;
+
+	// We won't delete this since it was created elsewhere
+	cMouseController = NULL;
+
+	// We won't delete this since it was created elsewhere
+	cKeyboardController = NULL;
+	
+	// We won't delete this since it was created elsewhere
+	cSettings = NULL;
+}
+
+/**
+ @brief Init Initialise this instance
+ @return true if the initialisation is successful, else false
+ */ 
+bool CScene3D::Init(void)
+{
+	cSettings = CSettings::GetInstance();
+
+	// configure global opengl state
+	glEnable(GL_DEPTH_TEST);
+	glEnable(GL_CULL_FACE);
+
+	// Configure the camera
+	cCamera = CCamera::GetInstance();
+	cCamera->vec3Position = glm::vec3(0.0f, 0.5f, 3.0f);
+
+	// Store the keyboard controller singleton instance here
+	cKeyboardController = CKeyboardController::GetInstance();
+
+	// Store the mouse controller singleton instance here
+	cMouseController = CMouseController::GetInstance();
+
+	// Store the CFPSCounter singleton instance here
+	cFPSCounter = CFPSCounter::GetInstance();
+
+	// Store the CSkyBox singleton instance here
+	cSkybox = CSkyBox::GetInstance();
+	cSkybox->SetShader("Shader3D_SkyBox");
+	cSkybox->Init();
+
+	// Store the CSkyBox singleton instance here
+	cTerrain = CTerrain::GetInstance();
+	cTerrain->SetShader("Shader3D_Terrain");
+	cTerrain->Init();
+	// Set the size of the terrain
+	cTerrain->SetRenderSize(100.f, 5.0f, 100.f);
+
+	// Load the sounds into CSoundController
+	cSoundController = CSoundController::GetInstance();
+	cSoundController->Init();
+	cSoundController->LoadSound(FileSystem::getPath("Sounds\\Sound_Bell.ogg"), 1, true);
+	cSoundController->LoadSound(FileSystem::getPath("Sounds\\Sound_Explosion.ogg"), 2, true);
+	cSoundController->LoadSound(FileSystem::getPath("Sounds\\Sound_Jump.ogg"), 3, true);
+
+	return true;
+}
+
+/**
+ @brief Update Update this instance
+ @param dElapsedTime A const double variable contains the time since the last frame
+ @return A bool variable
+*/
+bool CScene3D::Update(const double dElapsedTime)
+{
+	if (CKeyboardController::GetInstance()->IsKeyDown(GLFW_KEY_I))
+		cCamera->ProcessKeyboard(CCamera::CAMERAMOVEMENT::FORWARD, (float)dElapsedTime);
+	if (CKeyboardController::GetInstance()->IsKeyDown(GLFW_KEY_K))
+		cCamera->ProcessKeyboard(CCamera::CAMERAMOVEMENT::BACKWARD, (float)dElapsedTime);
+	if (CKeyboardController::GetInstance()->IsKeyDown(GLFW_KEY_J))
+		cCamera->ProcessKeyboard(CCamera::CAMERAMOVEMENT::LEFT, (float)dElapsedTime);
+	if (CKeyboardController::GetInstance()->IsKeyDown(GLFW_KEY_L))
+		cCamera->ProcessKeyboard(CCamera::CAMERAMOVEMENT::RIGHT, (float)dElapsedTime);
+
+	// Get mouse updates
+	cCamera->ProcessMouseMovement((float)cMouseController->GetMouseDeltaX(), (float)cMouseController->GetMouseDeltaY());
+	cCamera->ProcessMouseScroll((float)cMouseController->GetMouseScrollStatus(CMouseController::SCROLL_TYPE::SCROLL_TYPE_YOFFSET));
+
+	// Post Update the mouse controller
+	cMouseController->PostUpdate();
+
+	return true;
+}
+
+/**
+ @brief PreRender Set up the OpenGL display environment before rendering
+ */
+void CScene3D::PreRender(void)
+{
+	// Reset the OpenGL rendering environment
+	glLoadIdentity();
+
+	// Clear the screen and buffer
+	glClearColor(0.0f, 0.1f, 0.5f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+/**
+ @brief Render Render this instance
+ */
+void CScene3D::Render(void)
+{
+	glEnable(GL_DEPTH_TEST); // enable depth testing (is disabled for rendering screen-space quad)
+
+							 // Part 2: Render the entire scene as per normal
+	// Get the camera view and projection
+	glm::mat4 view = CCamera::GetInstance()->GetViewMatrix();;
+	glm::mat4 projection = glm::perspective(	glm::radians(CCamera::GetInstance()->fZoom),
+												(float)cSettings->iWindowWidth / (float)cSettings->iWindowHeight,
+												0.1f, 1000.0f);
+	glClearColor(0.0f, 0.0f, 0.5f, 1.0f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	//Render Skybox
+	cSkybox->SetView(view);
+	cSkybox->SetProjection(projection);
+	cSkybox->PreRender();
+	cSkybox->Render();
+	cSkybox->PostRender();
+
+	//Render Terrain
+	cTerrain->SetView(view);
+	cTerrain->SetProjection(projection);
+	cTerrain->PreRender();
+	cTerrain->Render();
+	cTerrain->PostRender();
+
+	glDisable(GL_DEPTH_TEST); // disable depth test so screen-space quad isn't discarded due to depth test.
+
+	return;
+}
+
+/**
+ @brief PostRender Set up the OpenGL display environment after rendering.
+ */
+void CScene3D::PostRender(void)
+{
+}
---
+++ App/Source/Scene3D/Scene3D.h
@@ -0,0 +1,90 @@
+/**
+ CScene3D
+ By: Toh Da Jun
+ Date: Mar 2020
+ */
+#pragma once
+
+// Include SingletonTemplate
+#include "DesignPatterns\SingletonTemplate.h"
+
+// Include GLM
+#include <includes/glm.hpp>
+#include <includes/gtc/matrix_transform.hpp>
+#include <includes/gtc/type_ptr.hpp>
+
+// Include CKeyboardController
+#include "Inputs\KeyboardController.h"
+// Include CMouseController
+#include "Inputs\MouseController.h"
+
+// Include CFPSCounter
+#include "TimeControl\FPSCounter.h"
+
+// Include CSoundController
+#include "..\SoundController\SoundController.h"
+
+// Include CCamera
+#include "Camera.h"
+
+// Include Skybox
+#include "Skybox/SkyBox.h"
+
+// Include Terrain
+#include "Terrain/Terrain.h"
+
+class CSettings;
+
+class CScene3D : public CSingletonTemplate<CScene3D>
+{
+	friend CSingletonTemplate<CScene3D>;
+public:
+	// Init
+	bool Init(void);
+
+	// Update
+	bool Update(const double dElapsedTime);
+
+	// PreRender
+	void PreRender(void);
+
+	// Render
+	void Render(void);
+
+	// PostRender
+	void PostRender(void);
+
+protected:
+	// The handler to the CSettings
+	CSettings* cSettings;
+
+	// Keyboard Controller singleton instance
+	CKeyboardController* cKeyboardController;
+
+	// Mouse Controller singleton instance
+	CMouseController* cMouseController;
+
+	// A transformation matrix for controlling where to render the entities
+	glm::mat4 transform;
+
+	// FPS Control
+	CFPSCounter* cFPSCounter;
+
+	// Handler to the CSoundController
+	CSoundController* cSoundController;
+
+	// Handler to the camera class
+	CCamera* cCamera;
+
+	// Handler to the skybox class
+	CSkyBox* cSkybox;
+
+	// Handler to the skybox class
+	CTerrain* cTerrain;
+
+	// Constructor
+	CScene3D(void);
+	// Destructor
+	virtual ~CScene3D(void);
+};
+
